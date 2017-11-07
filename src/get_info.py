e01973042e9798d0d012ae41f7e2e235c5d9aead
Jim Heald <james.r.heald@gmail.com>
2017-11-07 09:04:52 -0800

    Some optimizations and error checking

--- src/get_info.py
+++ src/get_info.py
@@ -1,3 +1,5 @@
+import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,29 +10,50 @@ def get_info(clses, profs=None):
     driver.get("https://pass.calpoly.edu")
 
     depts = [x.split()[0] for x in clses]
+    print(depts)
 
     dept_selector = driver.find_element_by_xpath("//select[@data-filter='dept']") 
+
+    num_courses = 0
     for option in dept_selector.find_elements_by_tag_name('option'):
-        for dept in depts:
-            if dept in option.text:
-                option.click()
-                course_list = driver.find_element_by_class_name("course-list")
-                
-                click_courses(driver, [x.split()[1] for x in clses if dept in x]) 
+        dept, ln = option.text.split("-")[:2]
+
+        if dept in depts:
+            print(option.text)
+            option.click()
+            course_list = driver.find_element_by_class_name("course-list")
+            
+            num_courses += click_courses(driver, [x.split()[1] for x in clses if dept in x], dept) 
 
     cart = driver.find_element_by_id("cart-list-view")
+    assert num_courses == len(list(cart.find_elements_by_class_name("clearfix")))
+
     for cls in cart.find_elements_by_class_name("clearfix"):
         print(cls.find_element_by_class_name("left").text)
 
-def click_courses(driver, courses):
+def click_courses(driver, courses, d):
+    print(courses)
+
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "selectCol"))
     )
+    # This is to wait for the table to fully load (the wait driver doesn't
+    # always work if it was clicked before
+    time.sleep(1)
+    driver.save_screenshot(f"{d}-buttons.png")
 
     course_list = driver.find_element_by_class_name("course-list")
     
     for row in course_list.find_elements_by_tag_name("tr"):
-        cols = list(row.find_elements_by_tag_name("td"))
-        if len(cols) > 5 and cols[2].text in courses:
-            cols[0].find_element_by_class_name("btn").click()
+        try:
+            cols = list(row.find_elements_by_tag_name("td"))
+            if len(cols) > 5 and cols[2].text in courses:
+                cols[0].find_element_by_class_name("btn").click()
+                return 1 
+        except:
+            driver.save_screenshot("screenshot.png")
+            driver.close()
+            sys.exit(1)
+    
+    return 0
 
