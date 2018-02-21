89589d9bd3a57b64587592ed00c5808e7d760a95
Jim Heald <james.r.heald@gmail.com>
2018-02-20 23:22:02 -0800

    Connect to and render the frontend

--- dontpass/pass_app/views.py
+++ dontpass/pass_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Class
 
 # Create your views here.
+def index(request):
+    context = {"classes": Class.objects.all()}
+    return render(request, 'pass_app/main.html', context)
